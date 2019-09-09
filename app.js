39e73c2c6a0bde2582ade4da93eb60eb2a7b7476
Yousef <yousef.saghir@outlook.com>
2019-09-09 17:31:39 +0200

    modified my project

--- app.js
+++ app.js
@@ -5,42 +5,42 @@ const myInfoCity = document.querySelectorAll(".info");
 const myFamousCity = document.querySelectorAll(".famous");
 
 myLinkCity.forEach(lin => {
-   lin.addEventListener("click", () => {
-      myCityName.textContent = lin.textContent;
-      myFeth(lin.textContent);
-   });
+  lin.addEventListener("click", () => {
+    myCityName.textContent = lin.textContent;
+    myFeth(lin.textContent);
+  });
 });
 
 function myFeth(discreption) {
-   fetch("city.json")
-      .then(res => {
-         return res.json();
-      })
-      .then(data => {
-         data.map(data => {
-            if (data.id == discreption) {
-               let i = 0;
-               myCityDisc.textContent = "";
-               var t = setInterval(() => {
-                  myCityDisc.textContent += data.disc[i];
+  fetch("city.json")
+    .then(res => {
+      return res.json();
+    })
+    .then(data => {
+      data.map(data => {
+        if (data.id == discreption) {
+          let i = 0;
+          myCityDisc.textContent = "";
+          var t = setInterval(() => {
+            myCityDisc.textContent += data.disc[i];
 
-                  if (i == data.disc.length - 1) {
-                     clearInterval(t);
-                  }
-                  i++;
-               }, 100);
+            if (i == data.disc.length - 1) {
+              clearInterval(t);
             }
-         });
+            i++;
+          }, 100);
+        }
       });
+    });
 }
 
 myFamousCity.forEach(d => {
-   d.addEventListener("mouseover", () => {
-      d.firstElementChild.className += " over";
-   });
+  d.addEventListener("mouseover", () => {
+    d.firstElementChild.className += " over";
+  });
 });
 myFamousCity.forEach(d => {
-   d.addEventListener("mouseleave", () => {
-      d.firstElementChild.classList.remove("over");
-   });
+  d.addEventListener("mouseleave", () => {
+    d.firstElementChild.classList.remove("over");
+  });
 });
